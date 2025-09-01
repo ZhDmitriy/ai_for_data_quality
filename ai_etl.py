60f77273a9e47209711e246f51ead85858f85776
Dmitriy <dmitriy@MacBook-Air-Dmitriy.local>
2025-09-01 22:42:00 +0300

    Структура проекта

---
+++ ai_etl.py
@@ -0,0 +1,49 @@
+"""
+    Применяем AI в ETL/ELT процессах для обеспечения высокого уровня качества данных
+"""
+
+
+class AiModelClient: 
+    """ Клиент по взаимодействию с LLM моделью DeepSeek-V3 """
+
+    def _init__(self):
+        pass 
+
+
+class PrecisionAiAgent:
+    """ ИИ агент для оценки точности данных """
+
+    def __init__(self):
+        pass 
+
+
+class FulnessAiAgent: 
+    """ ИИ агент для оценки полноты данных """
+
+    def __init__(self):
+        pass 
+
+
+class ConsistencyAiAgent:
+    """ ИИ агент для оценки согласованности данных """
+
+    def __init__(self): 
+        pass 
+
+
+class ValidityAiAgent:
+    """ ИИ агент для оценки достоверности данных """ 
+
+    def __init__(self):
+        pass 
+
+
+class RelevantAiAgent:
+    """ ИИ агент для оценки своевременности данных """
+
+    def __init__(self):
+        pass 
+
+
+if __name__ == '__main___': 
+    pass 
