a8a4928f2bad9830d669adaa2326d270c7feac95
Dmitriy <dmitriy@MacBook-Air-Dmitriy.local>
2025-09-01 23:50:37 +0300

    Update all files

--- ai_etl.py
+++ ai_etl.py
@@ -1,19 +1,45 @@
 """
-    Применяем AI в ETL/ELT процессах для обеспечения высокого уровня качества данных
+    Применяем AI в ETL/ELT процессах для обеспечения высокого уровня качества данных.
+
+    Пример рассмотрен на предметной области торговли на маркетплейсах - торговля в общем смысле. 
 """
+
+import pandas as pd 
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+class DataSourceClient:
+    """ Клиент управления источником данных. 
+        В этом классе созданы синтетические данные из 100 строк: 
+        - 80% корректные 
+        - 20% неккоректные 
+    """
+
+    def create_dataset(self) -> pd.DataFrame: 
+        """ Читаем датасет с данными """
+        data_ecom = pd.read_csv("data_ecom.csv", sep=';')
+        print(data_ecom)
 
 
 class AiModelClient: 
     """ Клиент по взаимодействию с LLM моделью DeepSeek-V3 """
 
     def _init__(self):
-        pass 
+        self.api_key = os.environ.get('DEEPSEEK_API_KEY')
 
 
 class PrecisionAiAgent:
     """ ИИ агент для оценки точности данных """
 
     def __init__(self):
+        pass 
+
+    def get_prompt_requests(self) -> pd.DataFrame: 
+        """ Получаем промпт запросы для оценки точности данных """
         pass 
 
 
@@ -24,10 +50,19 @@ class FulnessAiAgent:
         pass 
 
 
+    def get_prompt_requests(self) -> pd.DataFrame: 
+        """ Получаем промпт запрос для оценки полноты данных """
+        pass 
+
+
 class ConsistencyAiAgent:
     """ ИИ агент для оценки согласованности данных """
 
     def __init__(self): 
+        pass 
+
+    def get_prompt_requests(self) -> pd.DataFrame: 
+        """ Получаем промпт запрос для оценки согласованности данных """
         pass 
 
 
@@ -37,6 +72,10 @@ class ValidityAiAgent:
     def __init__(self):
         pass 
 
+    def get_prompt_requests(self) -> pd.DataFrame: 
+        """ Получаем промпт запрос для оценки достоверности данных """
+        pass 
+
 
 class RelevantAiAgent:
     """ ИИ агент для оценки своевременности данных """
@@ -44,6 +83,13 @@ class RelevantAiAgent:
     def __init__(self):
         pass 
 
+    def get_prompt_requests(self) -> pd.DataFrame: 
+        """ Получаем промпт запрос для оценки своерменности данных """
+        pass 
 
-if __name__ == '__main___': 
-    pass 
+
+if __name__ == '__main__': 
+    ai_model = AiModelClient()
+
+    data_source = DataSourceClient()
+    data_source.create_dataset()
